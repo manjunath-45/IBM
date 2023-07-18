c5c8284742320efc9476ac1f078e3409fe17a926
MANJUNATH  ACHARI S <93462181+manjunath-45@users.noreply.github.com>
2023-07-18 15:51:44 +0530

    Add files via upload

--- spring-project/src/main/java/org/example/model/Student.java
+++ spring-project/src/main/java/org/example/model/Student.java
@@ -1,10 +1,13 @@
 package org.example.model;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("s")
-@Scope("prototype")
+//@Scope("prototype")
 public class Student {
 	
 	private String name;
@@ -22,5 +25,15 @@ public class Student {
 		this.id = id;
 	}
 	
-     
+	@PostConstruct
+	public void initStudent()
+	{
+	   System.out.println("init method called...");
+	}
+	
+	@PreDestroy
+	public void destroyStudent()
+	{
+		System.out.println("destroy method called..");
+	}
 }
