a4ec22377f04c9db78307389e9b5694c327a485c
MANJUNATH  ACHARI S <93462181+manjunath-45@users.noreply.github.com>
2023-07-25 10:47:05 +0530

    Add files via upload

---
+++ employee-app/src/main/java/org/example/model/Employee.java
@@ -0,0 +1,43 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+	
+
+	private String name;
+	private int age;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public Employee(String name2, int age2) {
+		
+	}
+
+}
