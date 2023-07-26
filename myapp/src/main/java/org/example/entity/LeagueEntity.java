6a26f8ce2493b46056add2b9d6eabc6c7d5dc177
MANJUNATH  ACHARI S <93462181+manjunath-45@users.noreply.github.com>
2023-07-26 09:44:44 +0530

    Add files via upload

---
+++ myapp/src/main/java/org/example/entity/LeagueEntity.java
@@ -0,0 +1,34 @@
+package org.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "league_table")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LeagueEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int leagueId;
+	private int year;
+	private String season;
+	private String title;
+	
+	public LeagueEntity(int string, String season, String title) {
+		super();
+		this.year = string;
+		this.season = season;
+		this.title = title;
+}
+
+}
---
+++ myapp/src/main/java/org/example/model/League.java
@@ -0,0 +1,52 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class League {
+	
+	
+	
+	private int year;
+	private String season;
+	private String title;
+	
+	public League(int i, String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getSeason() {
+		return season;
+	}
+
+	public void setSeason(String season) {
+		this.season = season;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+   
+	
+}
